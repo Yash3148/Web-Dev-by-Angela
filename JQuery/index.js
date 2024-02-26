bb6e44cdad3be7ee8e9f69ce8f64dfecf7dca135
Yash <yashsingh3148@gmail.com>
2024-02-26 14:06:19 +0530

    Added Changes

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,13 +1,13 @@
-// alert("Working")
+alert("Working")
 
-// $("h1").addClass("big-title margin-50"); ---> Multiply Class can be added
+$("h1").addClass("big-title margin-50"); // ---> Multiply Class can be added
 
-// console.log($("img").attr("src"));
-// $("a").attr("href", "https://www.yahoo.com")
+console.log($("img").attr("src"));
+$("a").attr("href", "https://www.yahoo.com")
 
-// $("button").click(function(){
-//     $("h1").css("color", "purple");
-// });
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
 
 $(".btn2").click(function(){
     $("h1").css("color", "green");
@@ -24,7 +24,20 @@ $("input").keypress(function(event){
 $("h1").on("mouseover", function(event){
     $("h1").css("color", "red");
 });
-             
+      
+$("button").on("click", function(){
+    $("h1").hide();
+});
+
+$("button").on("click", function(){
+    $("h1").show();
+});
+
+$("button").on("click", function(){
+    $("h1").fadeOut();
+});
+
+
 // .addClass ---> To add a class.                   $("h1").addClass("big-title);
 
 // .removeClass ---> To remove a class              $("h1").removeClass("big-title");
@@ -39,20 +52,65 @@ $("h1").on("mouseover", function(event){
 
 // .click ---> Similar to click function in JS
 
-       // $("h1").click(function(){
-      //     $("h1").css("color", "purple");
-     //   });
+         $("h1").click(function(){
+           $("h1").css("color", "purple");
+         });
 
 // mouseover --> It is method through which when we take mouse on h1 it will change its color.
 
-           // $("h1").on("mouseover", function(event){
-          //     $("h1").css("color", "red");
-         // });
+         $("h1").on("mouseover", function(event){
+              $("h1").css("color", "red");
+         });  
     
-// .before ---> It will add bofore the query which has been selected  $("h1").before("<button>New</button>")
+// .before ---> It will add bofore the query which has been selected.  $("h1").before("<button>New</button>")
 
-// .after  ---> It will add after the query which has been selected   $("h1").after("<button>New</button>")
+// .after  ---> It will add after the query which has been selected.   $("h1").after("<button>New</button>")
 
-// .prepend --> It will add the just before the closing tag           $("h1").prepend("<button>New</button>")
+// .prepend --> It will add the just before the closing tag.           $("h1").prepend("<button>New</button>")
 
-// .append ---> It will add the just after the closing tag            $("h1").append("<button>New</button>")
+// .append ---> It will add the just after the closing tag.            $("h1").append("<button>New</button>")
+
+// .hide ---> It will hide the query which was selected.
+        $("button").on("click", function(){
+          $("h1").hide();
+        });
+
+//  .show ---> It will show the query which was selected as hidden.
+        $("button").on("click", function(){
+           $("h1").show();
+        });
+
+//  .toggle --> It will toggle the query which was selected.
+        $("button").on("click", function(){
+           $("h1").toggle();
+        });
+
+//  .fadeOut ---> It will hide but in fading way.
+        $("button").on("click", function(){
+           $("h1").fadeOut();
+        });
+
+//  .fadeIn ---> It will show but in fading way.
+        $("button").on("click", function(){
+           $("h1").fadeOut();
+        });
+
+//  .slideUp ---> It will show but in sliding way.
+        $("button").on("click", function(){
+           $("h1").slideUp();
+        });
+
+//  .slideDown ---> It will show but in sliding way.
+        $("button").on("click", function(){
+           $("h1").slideDown();
+        });
+
+//  .slidetoggle ---> It will toggle but in sliding way.
+        $("button").on("click", function(){
+           $("h1").toggle();
+        });
+
+//  .animate ---> It will help to provide animation.
+        $("button").on("click", function(){
+           $("h1").animate({opacity: 0.5});
+        });
