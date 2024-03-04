821a9306a9b99fab9d325d33cc54ae5c4337595b
Yash <yashsingh3148@gmail.com>
2024-03-04 18:34:56 +0530

    Tested

--- 9_NodeJS/NPM/index.js
+++ 9_NodeJS/NPM/index.js
@@ -1,6 +1,13 @@
+// This method is done through installing a package and working on that,
 var genrateName = require("sillyname")
 var sillyName = genrateName();
 
 console.log(`My name is ${sillyName}.`);
 
 // This will generate random names 
+
+// This method is done through module where we are calling a module through import
+import genrateName from "sillyname";
+var sillyName = genrateName();
+
+console.log(`My name is ${sillyName}.`);
