58a99dfba955319392c3095a36ad35db36ef2935
Yash <yashsingh3148@gmail.com>
2024-03-10 01:11:03 +0530

    ExpressJS

--- 10_ExpressJS/MiddleWares/Middleware/solution1.js
+++ 10_ExpressJS/MiddleWares/Middleware/solution1.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 res.sendFile(__dirname + "/public/index.html");
