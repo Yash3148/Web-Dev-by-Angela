232be7fb4746484ea7cd46d136e6da7ec80a3828
Yash <yashsingh3148@gmail.com>
2024-03-09 23:03:27 +0530

    ExpressJS

--- 10_ExpressJS/http_request/index.js
+++ 10_ExpressJS/http_request/index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {   // req --> request  , res --> response
     res.send("<h1>Hello Yash!!</h1>"); // this will show Hello world in chrome
 })
 
+app.get("/about", (req, res) => {   
+    res.send("<h1>About Me</h1><p>My name is yash</p>");  // http://localhost:3000/about
+})
+
+app.get("/contact", (req, res) => {    
+    res.send("<h1Contact me</h1><p>+91-123456433<p>");    // http://localhost:3000/contact
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
