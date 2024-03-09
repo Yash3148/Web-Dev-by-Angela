08e4a9c8b5fafb40147e549ce469116013aa593a
Yash <yashsingh3148@gmail.com>
2024-03-09 22:26:45 +0530

    ExpressJS

--- 10_ExpressJS/http_request/index.js
+++ 10_ExpressJS/http_request/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {   // req --> request  , res --> response
     console.log(req.rawHeaders);  // rawHeaders tells where the request is orignated from all the raw data of request.
-    res.send("Hello World!"); // this will show Hello world in chrome
+    res.send("<h1>Hello Yash!!</h1>"); // this will show Hello world in chrome
 })
 
 app.listen(port, () => {
