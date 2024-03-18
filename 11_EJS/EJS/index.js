6174bfc0723945e853d1291fe8c966d7697c259d
Yash <yashsingh3148@gmail.com>
2024-03-18 13:00:22 +0530

    EJS

--- 11_EJS/EJS/index.js
+++ 11_EJS/EJS/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
     adv = "it's time to have some fun";
   }
 
-  res.render("solution.ejs", {
+  res.render("index.ejs", {
     dayType: type,
     advice: adv,
   });
